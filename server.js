e669f3412571b9c33f782180004e35a0b245a9e2
chinaza-max <mosesogbonna68@gmail.com>
2022-01-20 05:12:30 -0800

    first commit

--- server.js
+++ server.js
@@ -52,12 +52,11 @@ app.use("/",router4);
 if(process.env.NOde_ENV==="production"){
 
   app.use(express.static("client/build"))
- /* app.use(express.static(path.join(__dirname, 'client', 'build')));
+ // app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
-*/
 }
 app.listen(port ,()=>console.log(`server started.... ${port}`))
 //chinaza100*
