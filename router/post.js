9d23fef90197e87573db556f6532d5f84de2896d
chinaza-max <mosesogbonna68@gmail.com>
2022-01-23 10:05:06 -0800

    test

--- router/post.js
+++ router/post.js
@@ -226,7 +226,7 @@ router.post('/uploadBook/:id',(req,res)=>{
                     file.tel=req.body.tel
                     file.date=Time().year + "-" + Time().month + "-" +Time().date
                     console.log("chinaza chinaza")
-                    console.log(`${__drname}`)
+                    console.log(`${__dirname}`)
                     console.log("chinaza chinaza chinaza")
                     file.mv( `${__dirname}../../client/public/uploads/${file.name}`,async(err)=>{
                         if(err){
