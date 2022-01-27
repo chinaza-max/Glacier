5c7e4f735e048340e05bf207f4c7540c7e68419a
chinaza-max <mosesogbonna68@gmail.com>
2022-01-26 20:49:57 -0800

    google drive

--- router/settings.js
+++ router/settings.js
@@ -7,7 +7,16 @@ const fs=require('fs');
 const {nameOfFiles,deleteAllFiles,deleteAllPDFFiles,deleteAllAccomodationFiles}=require("../deletefiles");
 const mongoConnection=require('mongoose')
 const connection=mongoConnection.connection;
-
+const {google} = require('googleapis');
+const oauth2Client=new google.auth.OAuth2(
+    process.env.GOOGLE_DRIVE_CLIENT_ID,
+    process.env.GOOGLE_DRIVE_CLIENT_SECRET,
+    process.env.GOOGLE_DRIVE_REDIRECT_URI
+)
+const drive=google.drive({
+    version:'v3',
+    auth:oauth2Client
+})
 
 
 
@@ -342,6 +351,10 @@ function deleteAllPostFromBook(){
         }
     })
 }
+
+
+
+
 
 async function deleteDriveFile(id){
     console.log("delete")
