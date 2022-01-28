b7ba6be8e9e1a96ed92fa855761a27d7e99abb18
chinaza-max <mosesogbonna68@gmail.com>
2022-01-28 07:57:42 +0100

    accomo

--- client/src/component/Accomodation/Accomodation.js
+++ client/src/component/Accomodation/Accomodation.js
@@ -148,7 +148,7 @@ function Accomodation(props){
                         return( 
                             <div key={data.unique} className="accomodation_body">
                                 <div className="img_container">
-                                    <img className="bodyImg" src={data.driveURL} alt={"/accomodationImg/firstImg.jpg"} onClick={()=>{ShowImage(data.name,data.Address,data.selection)}} /> 
+                                    <img className="bodyImg" src={data.driveURL} alt={"/accomodationImg/firstImg.jpg"} onClick={()=>{ShowImage(data.driveURL?data.driveURL:data.name,data.Address,data.selection)}} /> 
                                 </div>
                                 <div className="info1">
                                     <div className="accomodation_type">
