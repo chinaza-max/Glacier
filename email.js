54643d99cec10b3b01be67caedf417b88d49b8e5
chinaza-max <mosesogbonna68@gmail.com>
2022-02-01 21:58:11 +0100

    test

--- email.js
+++ email.js
@@ -2,14 +2,21 @@ require('dotenv').config();
 const nodemailer=require('nodemailer');
 
 
+
+
+
+let auth = {
+    type: 'oauth2',
+    user: 'brightmind.lmt@gmail.com',
+    clientId: '349313805948-fuc15p5cm97hr1nsme1bt55m32dtek7e.apps.googleusercontent.com',
+    clientSecret: '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',
+    refreshToken: '1//04SRSJKPMND2ECgYIARAAGAQSNwF-L9IrrVCA3QVfuS96o1pNQrY0ny_4NpzaF1QMTRx5LUtXs2M3i5HVN-hUZGiwmEu-HAxNcvQ',
+};
+
 const transporter=nodemailer.createTransport({
     service:'gmail',
-    auth:{
-        user: process.env.PASSWORD,
-        pass: process.env.EMAIL
-    }
+    auth:auth
 });
-
 
 const sendMail=(email,subject,text,id,cb)=>{
     const mailOptions={
